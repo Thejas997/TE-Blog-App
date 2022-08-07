bab6936f5e27c858138a14e7aeb11ac6f6600174
Thejas997 <s.thejas99@gmail.com>
2022-08-08 01:13:26 +0530

    Project Init

---
+++ src/main/java/com/springboot/TEBlogApp/TeBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.TEBlogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeBlogAppApplication.class, args);
+	}
+
+}
